6a93fdc62e3e4a6e0168ca8f2f78b6c69fcdddff
Pierre.Jacobs <pierre.jacobs@dyna-mo.com>
2025-09-30 23:01:17 +0200

    Start over

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"adventofcode/dayone"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "dayOne":
+		dayone.Challenge()
+	case "dayOneSimple":
+		dayone.ChallengeSimple()
+	default:
+		fmt.Println("Please provide a parameter to say which challenge to run.\nOptions examples: dayOne, dayOneSimple, dayTwo etc.")
+	}
+
+	fmt.Printf("Duration of run: %v", time.Since(start))
+}
